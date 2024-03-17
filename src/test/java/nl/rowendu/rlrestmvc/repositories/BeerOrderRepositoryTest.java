f8627f664730d308b452c649f201ac0a6d2b6c24
ronlangeveld <ron66langeveld@gmail.com>
2024-03-17 17:30:55 +0100

    103-rel-persisting-relationships

--- src/test/java/nl/rowendu/rlrestmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/nl/rowendu/rlrestmvc/repositories/BeerOrderRepositoryTest.java
@@ -3,38 +3,41 @@ package nl.rowendu.rlrestmvc.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import nl.rowendu.rlrestmvc.entities.Beer;
+import nl.rowendu.rlrestmvc.entities.BeerOrder;
 import nl.rowendu.rlrestmvc.entities.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class BeerOrderRepositoryTest {
-    @Autowired
-    BeerOrderRepository beerOrderRepository;
-    @Autowired
-    CustomerRepository customerRepository;
-    @Autowired
-    BeerRepository beerRepository;
-    
-    Customer testCustomer;
-    Beer testBeer;
-    
-    @BeforeEach
-    void setUp() {
-        testCustomer = customerRepository.findAll().getFirst();
-        testBeer = beerRepository.findAll().getFirst();
-    }
+  @Autowired BeerOrderRepository beerOrderRepository;
+  @Autowired CustomerRepository customerRepository;
+  @Autowired BeerRepository beerRepository;
 
-    @Test
-    void testBeerOrders() {
-    System.out.println(beerOrderRepository.count());
-    System.out.println(customerRepository.count());
-    System.out.println(beerRepository.count());
-    System.out.println(testCustomer.getName());
-    System.out.println(testBeer.getBeerName());
+  Customer testCustomer;
+  Beer testBeer;
 
-    assertThat(beerOrderRepository.count()).isZero();
+  @BeforeEach
+  void setUp() {
+    testCustomer = customerRepository.findAll().getFirst();
+    testBeer = beerRepository.findAll().getFirst();
+  }
+
+  @Transactional
+  @Test
+  void testBeerOrders() {
+    BeerOrder beerOrder =
+        BeerOrder.builder()
+                .customerRef("Test Order")
+                .customer(testCustomer)
+                .build();
+
+    BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+    System.out.println(savedBeerOrder.getCustomerRef());
+
+    assertThat(beerOrderRepository.count()).isEqualTo(1L);
   }
 }
