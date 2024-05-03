d0d64817ed06cd90bd80486ee2a55945288edfd7
ronlangeveld <ron66langeveld@gmail.com>
2024-05-03 11:59:14 +0200

    116-security-fix-remaining-tests

--- src/main/java/nl/rowendu/rlrestmvc/config/SpringSecConfig.java
+++ src/main/java/nl/rowendu/rlrestmvc/config/SpringSecConfig.java
@@ -5,8 +5,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecConfig {
@@ -14,8 +14,8 @@ public class SpringSecConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-        .httpBasic(withDefaults())
-        .csrf(AbstractHttpConfigurer::disable);
+            .httpBasic(withDefaults())
+            .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/api/**")));
     return http.build();
   }
 }
